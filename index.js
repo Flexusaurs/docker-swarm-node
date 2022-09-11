87785a239af585fa6ace7f31242e8ca663569860
Itay Flexusaurs <eitayhaim@gmail.com>
2022-09-11 09:09:08 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import address from "address";
 import Express from 'express';
+import os from 'os';
+
 
 const app = Express()
 const appId = process.env.APPID
@@ -9,19 +11,19 @@ console.log(appId)
 //grabbing local ip by specifying interface, family
 const connectionInterface = address.interface('IPv4');
 const localAddress = connectionInterface.address;
-
+const localhostname = os.hostname;
 
 //validating if interface is a virtual or bridged or correct interface
-connectionInterface.internal === true ? console.error():
-console.log(connectionInterface.address)    
+//connectionInterface.internal === true ? console.error():
+console.log(localAddress)    
 
 app.listen(8080, function(){
-    console.log("app running on port 80")
+    console.log("app running on port 8080")
 })
 app.set('view engine', 'pug')
 
 app.get('/', (req,res) => {
- res.render('index', {address: localAddress})    
+ res.render('index', {address: localAddress, hostname: localhostname})    
 })
 
 
